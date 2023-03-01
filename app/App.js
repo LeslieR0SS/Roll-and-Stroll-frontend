8303727e26e6761ff6a286d66dab2e568e5702cb
LeslieR0SS <laranibarpozo@cifpfbmoll.eu>
2023-03-01 12:07:29 +0100

    feat:

---
+++ app/App.js
@@ -0,0 +1,5 @@
+/* Creamos el componente principal */
+
+export function App(){
+    document.getElementById("root").innerHTML = '<h1>Wellcome to Roll & Stroll SPA</h1>';
+};
---
+++ app/index.js
@@ -0,0 +1,6 @@
+import {App} from "./App.js";
+
+const d = document;
+
+//Cuando cargue todo el contonido del DOM de la página, insertamo en el DOM nuebstro objeto App
+d.addEventListener("DOMContentLoaded", App);
