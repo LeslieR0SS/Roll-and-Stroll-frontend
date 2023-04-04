f3f6b190d5fd30dfce10f5712b36d91719deedaa
LeslieR0SS <laranibarpozo@cifpfbmoll.eu>
2023-04-04 18:14:27 +0200

    refactor: :recycle: 1st refactor code attemped - I've extract in const the cbsuccess and cberror handle actions

--- app/components/router.js
+++ app/components/router.js
@@ -4,95 +4,101 @@ import { BikeCard } from "./bikeCard.js";
 import { Bike } from "./bike.js";
 import { StoreCard } from "./storeCard.js";
 
-export async function Router () {
-    const d  = document;
-    const w = window;
-    const $main = d.getElementById("main");
+export async function Router() {
+  const d = document;
+  const w = window;
+  const mainEl = d.getElementById("main");
 
+  // Destructuring
+  const { hash } = location;
+  console.log(hash);
 
-    // destructuring
-    let { hash } = location;
-    console.log(hash);
+  mainEl.innerHTML = "";
 
-    $main.innerHTML = null;
-
-    if(!hash || hash === "#/"){
-        //$main.innerHTML = "<h2>Sección del Home</h2>";
-        await ajax({
-            url:api.BIKES,
-            cbSuccess:(bikes) => {
-                console.log(bikes);
-                let html = "";
-                bikes.forEach((bike) => (html += BikeCard(bike)));
-                d.querySelector(".loader").style.display="none"; 
-                $main.innerHTML = html;
-            },
-        });
-        console.log(api.BIKE)
-    } else if (hash.includes("#/stores")){
-        $main.innerHTML = "<h2>Sección de las tiendas</h2>";
-        await ajax({
-            url:api.STORES,
-            //cbSuccess: (hello) => console.log(hello)
-            cbSuccess:(res) => {
-                console.log(res);
-                let html = "";
-                const stores = res.stores;
-                console.log(stores);
-                stores.forEach((store) => (html += StoreCard(store)));
-                d.querySelector(".loader").style.display="none"; 
-                $main.innerHTML = html;
-            },
-            cbError: (error) => {
-                console.error(error);
-                $main.innerHTML = "<p>Error loading stores</p>";
-            },
-            });
-
-    } else if (hash.includes( "#/spa/store/")){
-        //Aquí falla!!
-        console.log(`${api.STORE_BIKES}/${localStorage.getItem("store-card")}/bikes`);
-        await ajax({
-            url:`${api.STORE_BIKES}/${localStorage.getItem("store-card")}/bikes`,
-            cbSuccess:(storeBikes) => {
-                console.log(storeBikes);
-                let html = "";
-                storeBikes.forEach((storeBikes) => (html += BikeCard(storeBikes)));
-                d.querySelector(".loader").style.display="none"; 
-                $main.innerHTML = html;
-            },
-        });
-
-    } else if (hash.includes("#/spa/availables")){
-        $main.innerHTML = "<h2>Sección de Bicis disponibles </h2>";
-        console.log(`${api.BIKE_AVAILABLE}`)
-        await ajax({
-            url:api.BIKE_AVAILABLE,
-            cbSuccess:(bikes) => {
-                console.log(bikes);
-                let html = "";
-                bikes.forEach((bike) => (html += BikeCard(bike)));
-                d.querySelector(".loader").style.display="none"; 
-                $main.innerHTML = html;
-            },
-        });
-
-
-    }else{
-/*      $main.innerHTML = 
-        "<h2>Aquí cargará el contenido de la bici seleccionada</h2>"; */
-        console.log(`${api.BIKE}/${localStorage.getItem("bike-id")}`);
-        await ajax({
-            url:`${api.BIKE}/${localStorage.getItem("bike-id")}`,
-            cbSuccess:(bike) => {
-                console.log(bike);  
-                $main.innerHTML= Bike(bike);
-            },
-        });
-    }
-    
-    // para ocultar el componente loader en todas las secciones una vez se haya cargado el contenido.
+  const hideLoader = () => {
     d.querySelector(".loader").style.display = "none";
+  };
 
+  if (!hash || hash === "#/") {
+    //$main.innerHTML = "<h2>Sección del Home</h2>";
+    const handleSuccess = (bikes) => {
+      console.log(bikes);
+      const bikeCards = bikes.map((bike) => BikeCard(bike)).join("");
+      hideLoader();
+      mainEl.innerHTML = bikeCards;
+    };
 
+    await ajax({
+      url: api.BIKES,
+      cbSuccess: handleSuccess,
+    });
+
+    console.log(api.BIKE);
+  } else if (hash.includes("#/stores")) {
+    mainEl.innerHTML = "<h2>Sección de las tiendas</h2>";
+
+    const handleSuccess = (res) => {
+      console.log(res);
+      const storeCards = res.stores.map((store) => StoreCard(store)).join("");
+      hideLoader();
+      mainEl.innerHTML = storeCards;
+    };
+
+    const handleError = (error) => {
+      console.error(error);
+      mainEl.innerHTML = "<p>Error loading stores</p>";
+    };
+
+    await ajax({
+      url: api.STORES,
+      cbSuccess: handleSuccess,
+      cbError: handleError,
+    });
+  } else if (hash.includes("#/spa/store/")) {
+    //Aquí falla!!
+    console.log(
+      `${api.STORE_BIKES}/${localStorage.getItem("store-card")}/bikes`
+    );
+
+    const handleSuccess = (storeBikes) => {
+      console.log(storeBikes);
+      const bikeCards = storeBikes.map((bike) => BikeCard(bike)).join("");
+      hideLoader();
+      mainEl.innerHTML = bikeCards;
+    };
+
+    await ajax({
+      url: `${api.STORE_BIKES}/${localStorage.getItem("store-card")}/bikes`,
+      cbSuccess: handleSuccess,
+    });
+  } else if (hash.includes("#/spa/availables")) {
+    mainEl.innerHTML = "<h2>Sección de Bicis disponibles </h2>";
+    console.log(api.BIKE_AVAILABLE);
+
+    const handleSuccess = (bikes) => {
+      console.log(bikes);
+      const bikeCards = bikes.map((bike) => BikeCard(bike)).join("");
+      hideLoader();
+      mainEl.innerHTML = bikeCards;
+    };
+
+    await ajax({
+      url: api.BIKE_AVAILABLE,
+      cbSuccess: handleSuccess,
+    });
+  } else {
+    /*$main.innerHTML = 
+        "<h2>Aquí cargará el contenido de la bici seleccionada</h2>";*/
+    console.log(`${api.BIKE}/${localStorage.getItem("bike-id")}`);
+
+    const handleSuccess = (bike) => {
+      console.log(bike);
+      mainEl.innerHTML = Bike(bike);
+    };
+
+    await ajax({
+      url: `${api.BIKE}/${localStorage.getItem("bike-id")}`,
+      cbSuccess: handleSuccess,
+    });
+  }
 }
